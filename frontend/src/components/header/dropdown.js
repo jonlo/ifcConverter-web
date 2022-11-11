29f74a0d36d845707a9ee9b6617a0d5a5140a8d5
Jon Lopez de Guereña <jon.lopezdeguerena@tecnalia.com>
2022-11-11 10:20:57 +0100

    dropdown working

--- frontend/src/components/header/dropdown.js
+++ frontend/src/components/header/dropdown.js
@@ -3,15 +3,32 @@ import React from 'react';
 
 export function Dropdown(props) {
 
+	const [items, updateItems] = React.useState(props.children);
+
+	const toggleSelected = (element) => {
+
+		const selectedItem = items.find((i) => i.id === element.target.innerHTML);
+		selectedItem.selected = !selectedItem.selected;
+
+		if (!props.multiSelection) {
+			items.forEach(item => {
+				if (selectedItem.id !== item.id) {
+					item.selected = false;
+				}
+			});
+		}
+		updateItems([...items]);
+	};
+
 	return (
 		<div className="dropdown">
 			<span>{props.title}</span>
 			<div className="dropdown-content">
-				{props.children.map((child, index) => {
-					return <p key={index}>{child.id}</p>
+				{items.map((child, index) => {
+					return <p key={index} onClick={toggleSelected} data-selected={child.selected}>{child.id}</p>
 				})}
 			</div>
-		</div>
+		</div >
 	);
 
 }
