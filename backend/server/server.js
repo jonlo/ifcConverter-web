d74f9e11f47c4f48fc2065a2b351963e9de8a55c
Jon Lopez de guereña <jon.ldg85@gmail.com>
2022-11-15 20:12:58 +0100

    adding tests for the api calls

--- backend/server/server.js
+++ backend/server/server.js
@@ -13,7 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(require ('./routes/index'));
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`listening in port ${process.env.PORT}`)
 })
 
+module.exports = server
---
+++ backend/tests/apitest.test.js
@@ -0,0 +1,54 @@
+const server = require('../server/server.js');
+const supertest = require('supertest');
+const requestWithSupertest = supertest(server);
+
+describe('User Endpoints', () => {
+
+  it('POST /convert convert ifc to dae', async () => {
+    const res = await requestWithSupertest.post('/convert').send({ options: [], file: "haus.ifc", outputFile: "haus.dae" });
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+    expect(res.body).toHaveProperty('file')
+  });
+
+  it('POST /convert convert null', async () => {
+    const res = await requestWithSupertest.post('/convert').send({ options: [], file: "", outputFile: "haus.dae" });
+    expect(res.status).toEqual(500);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+  });
+
+  it('POST /convert convert null', async () => {
+    const res = await requestWithSupertest.post('/convert').send({ options: null, file: null, outputFile: "haus.dae" });
+    expect(res.status).toEqual(500);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+  });
+
+  it('get /download file', async () => {
+    const res = await requestWithSupertest.get('/download').query({ file: 'haus.ifc' });
+    expect(res.status).toEqual(200);
+  });
+
+  it('get /download file null', async () => {
+    const res = await requestWithSupertest.get('/download').query({ file: null });
+    expect(res.status).toEqual(404);
+  });
+
+  it('get /download file blank', async () => {
+    const res = await requestWithSupertest.get('/download').query({ file: '' });
+    expect(res.status).toEqual(404);
+  });
+
+  // it('POST /upload file', async () => {
+  //   const res = await requestWithSupertest.post('/upload').attach('file', './tests/haus.ifc')
+  //   expect(res.status).toEqual(200);
+  //   // expect(res.type).toEqual(expect.stringContaining('json'));
+  // });
+
+
+});
+
+
+afterAll(done => {
+  server.close();
+  done();
+});
